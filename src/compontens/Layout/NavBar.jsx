fd6bb268baf0df9446e9e020844028e2887f99ef
Alfie134 <alberteoegendahl@gmail.com>
2026-07-28 18:08:45 +0100

    NavBar 1st edition made and added

--- src/compontens/Layout/NavBar.jsx
+++ src/compontens/Layout/NavBar.jsx
@@ -1,7 +1,23 @@
 import './NavBar.css';
 
 export default function NavBar() {
+
+
     return(
-        <spacer></spacer>
+        <main className='navbar-cmp'>
+            <div className='content'>
+                <div className='greeting'>
+                    <h2> Hello, username</h2>
+                </div>
+                <div className='nav-links'>
+                    <a href='library'> library </a>
+                    <a href='journal'> journal </a>
+                    <a href='stats'> stats </a>
+                    <a href='tbr'> tbr </a>
+                </div>
+                <div className='profile'></div>
+            </div>
+            <hr className='navbar-spacer'></hr>
+        </main>
     );
 }     
