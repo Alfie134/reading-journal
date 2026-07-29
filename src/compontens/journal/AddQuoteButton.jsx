511ea942e2de3ebb11bd025f6a79b83cd43fb17e
LuukvEwijk <vanewijkluuk@gmail.com>
2026-07-29 20:46:25 +0200

    created button and changes quotecard

---
+++ src/compontens/journal/AddQuoteButton.jsx
@@ -0,0 +1,9 @@
+import './AddQuoteButton.css'
+
+export default function AddQuoteButton({text, source}) {
+    return (
+        <div className='addquote'>
+            <button>add quote</button>
+        </div>
+    );
+}
--- src/pages/BookDetailPage.jsx
+++ src/pages/BookDetailPage.jsx
@@ -1,3 +1,5 @@
+import QuoteCard from '../compontens/journal/QuoteCard';
+import AddQuoteButton from '../compontens/journal/AddQuoteButton';
 import './BookDetailPage.css';
 
 
@@ -5,12 +7,23 @@ export default function BookDetailPage() {
     return (
         <main className='book-detail-page'>
             <section className='book-detail'>
-                <div className='book-detail-hero'></div>
+                <div className='general-book'></div>
                 <div className='book-detail-info'>
-                    <h1>Book Title</h1>
+                    <p>series - 1 of 1 - finished</p>
+                    <h1>MockBook</h1>
                     <p>Author</p>
-                    <p>Genre</p>
-                    <p>Year Published</p>
+                    <p>stars + time spend + pages</p>
+                    <p>Genres</p>
+                </div>
+                <hr />
+            </section>
+            <section className='book-detail-journal'>
+                <div className='journal-entries'>
+                    <h2>Journal Entry</h2>
+                    <p>Entry 1</p>
+                    <QuoteCard text={"\"Beauty is terror. Whatever we call beautiful, we quiver before it.\""} source={"p. 200"}></QuoteCard>
+                    <QuoteCard text={"\"It's a rare thing to know your own heart.\""} source={"p. 1"}></QuoteCard>
+                    <AddQuoteButton/> 
                 </div>
             </section>
         </main>
