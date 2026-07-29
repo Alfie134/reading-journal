fe8dca038d8f4e27928595c447fba224e0519a0e
Alfie134 <alberteoegendahl@gmail.com>
2026-07-29 22:26:26 +0100

    mock books updated and book covers added

--- src/data/MockBook.jsx
+++ src/data/MockBook.jsx
@@ -1,26 +1,38 @@
 const MockBooks = [
   {
-    id: 1,
+    id: "1",
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
     genre: 'Fiction',
-    yearPublished: 1925
+    yearPublished: 1925,
+    cover: '',
+    pages: 200,
+    hoursLogged: '',
+    status: ''
 },
 
 {
-  id: 2,
+  id: "2",
   title: 'Alchemised',
   author: 'SenLinYu',
   genre: 'Dark Fantasy', 
-  yearPublished: 2025
+  yearPublished: 2025,
+  cover: './alchemised.jpg',
+  pages: 1040,
+  hoursLogged: '',
+  status: ''
 },
 
 {
-  id: 3,
+  id: "3",
   title: 'The Poppy War',
   author: 'R.F. Kuang',
   genre: 'High Fantasy', 
-  yearPublished: 2018
+  yearPublished: 2018,
+  cover: './ThePoppyWar.jpg',
+  pages: 530,
+  hoursLogged: '',
+  status: ''
 }
 ];
 export default MockBooks;
