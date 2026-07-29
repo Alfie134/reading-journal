ffb7d6e9af2bb43614d5ac705471178fca4ac3f7
Alfie134 <alberteoegendahl@gmail.com>
2026-07-29 21:51:24 +0100

    two additional mock books added

--- src/data/MockBook.jsx
+++ src/data/MockBook.jsx
@@ -5,6 +5,22 @@ const MockBooks = [
     author: 'F. Scott Fitzgerald',
     genre: 'Fiction',
     yearPublished: 1925
+},
+
+{
+  id: 2,
+  title: 'Alchemised',
+  author: 'SenLinYu',
+  genre: 'Dark Fantasy', 
+  yearPublished: 2025
+},
+
+{
+  id: 3,
+  title: 'The Poppy War',
+  author: 'R.F. Kuang',
+  genre: 'High Fantasy', 
+  yearPublished: 2018
 }
 ];
 export default MockBooks;
